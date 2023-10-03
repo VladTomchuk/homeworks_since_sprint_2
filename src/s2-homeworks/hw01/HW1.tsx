5443c63130e170be6ee7a8974c13a214d5da363e
VladTomchuk <tomchukvlad92@gmail.com>
2023-10-03 16:33:37 +0200

    Updated types for components and return info of messages

--- src/s2-homeworks/hw01/HW1.tsx
+++ src/s2-homeworks/hw01/HW1.tsx
@@ -14,7 +14,21 @@ import avatar from './avatar.png'
 * */
 
 // нужно создать правильный тип вместо any
-export type MessageType = any
+type UserType = {
+    avatar: string
+    name: string
+}
+
+type MessageBodyType = {
+    text: string
+    time: string
+}
+
+export type MessageType = {
+    id: number
+    user: UserType
+    message: MessageBodyType
+}
 
 // структуру объекта не менять
 export const message0: MessageType = {
